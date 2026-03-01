f51efb99b1faf70ea8f37bc15265ab58684deb6e
Fahad Masood <masoodfahad007@gmail.com>
2026-03-01 09:01:52 -0500

    New files including .env file so that whoever pulls code can setup their own databases and that way it isn't shared and security hazard is prevented. Make sure you setup your own .env file and database connection

--- Backend/backend/Store/db.js
+++ Backend/backend/Store/db.js
@@ -1,14 +1,14 @@
+require("dotenv").config();
 const mysql = require("mysql2/promise");
 
-// Connection pool (best practice for production)
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "your_password",
-  database: "payroll_system",
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = pool;
+
+
