77a85a5669015b466249fd64d39bacba245527b6
tasnimhanaif <tasnim.hanaif@gmail.com>
2026-03-22 19:46:51 -0400

    newest updates 3/22

--- Backend-updated/Store/db.js
+++ Backend-updated/Store/db.js
@@ -1,5 +1,24 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const mysql = require("mysql2/promise");
+
+function getSslConfig() {
+  const certificatePath = process.env.DB_SSL_CA_PATH;
+
+  if (!certificatePath) {
+    return undefined;
+  }
+
+  const resolvedCertificatePath = path.isAbsolute(certificatePath)
+    ? certificatePath
+    : path.resolve(__dirname, "..", certificatePath);
+
+  return {
+    ca: fs.readFileSync(resolvedCertificatePath, "utf8"),
+    rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED !== "false",
+  };
+}
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
@@ -7,8 +26,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: getSslConfig(),
 });
 
 module.exports = pool;
-
-
